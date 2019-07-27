e95a22d2b34bf5fa1fa7418dbe79aca4e1a21a28
Ivan Donato <donato.ivan@gmail.com>
2019-07-27 13:57:03 +1000

    added landing method

--- robot.rb
+++ robot.rb
@@ -7,4 +7,26 @@ class Robot
     @y_coordinate = 0
     @placed = false
   end
+
+   # display opening image
+   def landing
+    puts `clear`
+    print "\n\n"
+    print ".-----.            .---.       .-.         .-. \n"
+    print "`-. .-'            : .; :      : :        .' `.\n"
+    print "  : : .--. .-..-.  :   .' .--. : `-.  .--.`. .'\n"
+    print "  : :' .; :: :; :  : :.`.' .; :' .; :' .; :: : \n"
+    print "  :_;`.__.'`._. ;  :_;:_;`.__.'`.__.'`.__.':_; \n"
+    print "            .-. :                              \n"
+    print "            `._.'                              \n"
+    print " .--.  _                .-.         .-.         \n"  
+    print ": .--':_;               : :        .' `.         \n" 
+    print "`. `. .-.,-.,-.,-..-..-.: :   .--. `. .'.--. .--. \n"
+    print " _`, :: :: ,. ,. :: :; :: :_ ' .; ; : :' .; :: ..'\n"
+    print "`.__.':_;:_;:_;:_;`.__.'`.__;`.__,_;:_;`.__.':_;  \n\n\n"                                                   
+    print "Welcome to the Toy Robot Simulator!\n\n"
+    print "This program is a lightweight simulator of a toy robot moving on a square tabletop.\n"
+    print "Instruct your robot to move via commands in the CLI.\n"
+    print "To continue, please select one of the following options:\n\n"
+  end
 end
